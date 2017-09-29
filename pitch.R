d61df2b2f2be541ecd72855044709971295f5f07
SPFL-Analysis <31936950+SPFL-Analysis@users.noreply.github.com>
2017-09-29 15:25:16 +0100

    Update pitch.R
    Added centre circle perimeter

--- pitch.R
+++ pitch.R
@@ -1,7 +1,3 @@
-#' ggplot football pitch
-#' just done in 5mins at lunch
-#' possible to add centre circles with geom_path, 
-#' should be possible to draw arcs outside penalty area first, then draw penalty area over the top with 'fill = "#FFFFFF"'
 ggplot() +
   geom_rect(aes(xmin = 0, xmax = 100, ymin = 0, ymax = 100), fill = "green4", colour = "white", size = 1) +
   geom_rect(aes(xmin = 0, xmax = 50, ymin = 0, ymax = 100), fill = NA, colour = "white", size = 1) +
@@ -14,7 +10,8 @@ ggplot() +
   geom_point(aes(x=13, y=50),fill="white",colour="white",shape = 21, size = 1.5, stroke =1)+
   geom_point(aes(x=87, y=50),fill="white",colour="white",shape = 21, size = 1.5, stroke =1)+
   geom_curve(aes(x=17, y=38, xend = 17, yend=62),colour="white",size=1, curvature = 0.5)+
-  geom_curve(aes(x=83, y=38, xend = 83, yend=62),colour="white",size=1, curvature = -0.5)+
+  geom_curve(aes(x=50, y=40, xend = 50, yend=60),colour="white",size=1, curvature = 1)+
+  geom_curve(aes(x=50, y=40, xend = 50, yend=60),colour="white",size=1, curvature = -1)+
   theme(rect = element_blank(), 
         line = element_blank(),
         text = element_blank())
